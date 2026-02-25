ad6ddbeaf638fda666bd3e1fa8a1e78947772f82
farhan <112310170@global.ac.id>
2026-02-25 15:19:33 +0700

    updte home lagi fix

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -10,3 +10,9 @@ class MyHomePage extends StatefulWidget {
    @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
