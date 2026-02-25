bbd5795f8955682f36949baddb4f3a7d46c47d40
farhan <112310170@global.ac.id>
2026-02-25 15:18:23 +0700

    updte home lagi

---
+++ lib/view/home_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../models/counter_model.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
