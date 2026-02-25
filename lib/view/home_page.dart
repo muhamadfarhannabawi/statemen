115b03188a835cdbedad0eb0f13bb6ce5d7c12d2
farhan <112310170@global.ac.id>
2026-02-25 15:18:57 +0700

    updte home lagi 1

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -6,3 +6,7 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
+   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
