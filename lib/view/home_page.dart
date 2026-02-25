9a5e844cd735cbbcdb660cfac4263ba60e124be2
farhan <112310170@global.ac.id>
2026-02-25 15:20:52 +0700

    tambah body center

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -19,3 +19,11 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+
+       body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
